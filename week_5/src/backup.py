42eb491f37482ad9f3abc59080de66c133d7ffab
Anup Kumar <anup.rulez@gmail.com>
2022-05-31 12:42:22 +0200

    add backup

--- week_5/src/backup.py
+++ week_5/src/backup.py
@@ -22,26 +22,6 @@ cancer_names = ["blca", "brca", "coad", "hnsc", "ucec"]
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7998488/
 
 
-def visualize_graph(G, color):
-    plt.figure(figsize=(7,7))
-    plt.xticks([])
-    plt.yticks([])
-    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
-                     node_color=color, cmap="Set2")
-    plt.show()
-
-
-def visualize_embedding(h, color, epoch=None, loss=None):
-    plt.figure(figsize=(7,7))
-    plt.xticks([])
-    plt.yticks([])
-    h = h.detach().cpu().numpy()
-    plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
-    if epoch is not None and loss is not None:
-        plt.xlabel(f'Epoch: {epoch}, Loss: {loss.item():.4f}', fontsize=16)
-    plt.show()
-
-
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -89,8 +69,10 @@ def read_files():
     print(passengers)
     print("----")
     driver_gene_list = driver[0].tolist()
+    #print(driver_gene_list)
     passenger_gene_list = passengers[0].tolist()
     
+
     x, mapping = load_node_csv(final_path + "gene_features", 0)
     y = torch.zeros(x.shape[0], dtype=torch.long)
     y[:] = -1
@@ -98,49 +80,80 @@ def read_files():
     driver_ids = driver.replace({0: mapping})
     passenger_ids = passengers.replace({0: mapping})
 
-    # driver = 1, passenger = 0
+    '''print(driver_ids, driver_ids[0].tolist())
+    print()
+    print(passenger_ids, passenger_ids[0].tolist())'''
+
     y[driver_ids[0].tolist()] = 1
     y[passenger_ids[0].tolist()] = 0
-
+    #print(y[:, 737])
+    #print(y[:, 13255])
     print(y, y.shape)
+
+    #sys.exit()
+    # driver = 1, passenger = 0
+
+    '''driver_mask = gene_features[gene_features[0].isin(driver_gene_list)]
+    #print(driver_mask)
+    driver_y = [1 for item, i in driver_mask.iterrows()]
+    #print("----")
+    passenger_mask = gene_features[gene_features[0].isin(passenger_gene_list)]
+    passenger_y = [0 for item, i in passenger_mask.iterrows()]
+    #print(passenger_mask)
+    #print("----")
+    driver_y.extend(passenger_y)
+    #print("---------------")
+
+    y = torch.tensor(driver_y, dtype=torch.float)'''
 
     print("Saving mapping...")
     with open('gene_mapping.json', 'w') as outfile:
         outfile.write(json.dumps(mapping))
 
     print("replacing gene ids")
-    links = links[:500]
+    links = links[:5000]
     re_links = links.replace({0: mapping})
     re_links = re_links.replace({1: mapping})
     print(re_links)
 
+    '''driver_ids = driver.replace({0: mapping})
+    passenger_ids = passengers.replace({0: mapping})
+
+    #print(driver_ids, driver_ids[0].tolist())
+    #print()
+    #print(passenger_ids, passenger_ids[0].tolist())
+
+    #print(re_links)
+    re_links = re_links[re_links[0].isin(driver_ids[0].tolist()) & re_links[1].isin(passenger_ids[0].tolist())]
+    print(re_links)'''
+    #sys.exit()
+
     links_mat = re_links.to_numpy()
 
     # create data object
+    edge_index = torch.tensor(links_mat, dtype=torch.long) 
+    '''combined_tr = pd.concat([driver_mask, passenger_mask])
+    print(combined_tr)
+    combined_tr = combined_tr.loc[:, 1:]
+    print(combined_tr)
+    combined_tr = combined_tr.to_numpy()'''
     x = x.loc[:, 1:]
     x = x.to_numpy()
     x = torch.tensor(x, dtype=torch.float)
-    edge_index = torch.tensor(links_mat, dtype=torch.long) 
     data = Data(x=x, edge_index=edge_index.t().contiguous())
 
     #print(gene_features[0])
     print("create mask...")
+    #tr_mask_drivers = gene_features[0].isin(total_gene_list) #gene_features[0].isin(driver_gene_list) | gene_features[0].isin(passenger_gene_list)
+    #tr_mask_drivers = torch.tensor(tr_mask_drivers, dtype=torch.bool)
     driver_gene_list.extend(passenger_gene_list)
-    tr_mask_drivers = gene_features[0].isin(driver_gene_list)
+    tr_mask_drivers = gene_features[0].isin(driver_gene_list) #gene_features[0].isin(driver_gene_list) | gene_features[0].isin(passenger_gene_list)
     tr_mask_drivers = torch.tensor(tr_mask_drivers, dtype=torch.bool)
     data.train_mask = tr_mask_drivers
     data.y = y
 
     print(data)
-
-    # plot original graph
-    G = to_networkx(data, to_undirected=True)
-    visualize_graph(G, color=data.y)
-
-    sys.exit()
-
     model = GCN()
-
     print(model)
     criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
@@ -149,6 +162,7 @@ def read_files():
         loss, h = train(data, optimizer, model, criterion)
         if epoch % 10 == 0:
             print("Loss after {} epochs: {}".format(str(epoch), str(loss)))
+
 
 
 def train(data, optimizer, model, criterion):
