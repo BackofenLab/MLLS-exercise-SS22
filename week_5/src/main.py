b8a8b6b0a0e8bbed3dc1c85f48ac205297b361f5
Anup Kumar <anup.rulez@gmail.com>
2022-05-31 12:27:58 +0200

    update

--- week_5/src/main.py
+++ week_5/src/main.py
@@ -11,6 +11,8 @@ from torch_geometric.utils import add_self_loops, degree
 from torch_geometric.nn import knn_graph
 from torch_geometric.data import Data
 from torch_geometric.utils import to_networkx
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
 
 
 local_path = "../week_5/"
@@ -19,13 +21,27 @@ cancer_names = ["blca", "brca", "coad", "hnsc", "ucec"]
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7998488/
 
 
-def visualize_graph(G, color):
-    plt.figure(figsize=(7,7))
-    plt.xticks([])
-    plt.yticks([])
-    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
-                     node_color=color, cmap="Set2")
-    plt.show()
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(1234)
+        self.conv1 = GCNConv(12, 4)
+        self.conv2 = GCNConv(4, 4)
+        self.conv3 = GCNConv(4, 2)
+        self.classifier = Linear(2, 2)
+
+    def forward(self, x, edge_index):
+        h = self.conv1(x, edge_index)
+        h = h.tanh()
+        h = self.conv2(h, edge_index)
+        h = h.tanh()
+        h = self.conv3(h, edge_index)
+        h = h.tanh()  # Final GNN embedding space.
+        
+        # Apply a final (linear) classifier.
+        out = self.classifier(h)
+
+        return out, h
 
 
 def load_node_csv(path, index_col, encoders=None, **kwargs):
@@ -57,45 +73,76 @@ def read_files():
     
 
     x, mapping = load_node_csv(final_path + "gene_features", 0)
+    y = torch.zeros(x.shape[0], dtype=torch.long)
+    
+
+    driver_ids = driver.replace({0: mapping})
+    passenger_ids = passengers.replace({0: mapping})
+
+    '''print(driver_ids, driver_ids[0].tolist())
+    print()
+    print(passenger_ids, passenger_ids[0].tolist())'''
+
+    y[driver_ids[0].tolist()] = 1
+    y[passenger_ids[0].tolist()] = 0
+    #print(y[:, 737])
+    #print(y[:, 13255])
+    print(y, y.shape)
+
+    #sys.exit()
     # driver = 1, passenger = 0
 
-    driver_mask = gene_features[gene_features[0].isin(driver_gene_list)]
-    print(driver_mask)
+    '''driver_mask = gene_features[gene_features[0].isin(driver_gene_list)]
+    #print(driver_mask)
     driver_y = [1 for item, i in driver_mask.iterrows()]
-    print("----")
+    #print("----")
     passenger_mask = gene_features[gene_features[0].isin(passenger_gene_list)]
     passenger_y = [0 for item, i in passenger_mask.iterrows()]
-    print(passenger_mask)
-    print("----")
+    #print(passenger_mask)
+    #print("----")
     driver_y.extend(passenger_y)
-    print("---------------")
+    #print("---------------")
 
-    y = torch.tensor(driver_y, dtype=torch.float)
+    y = torch.tensor(driver_y, dtype=torch.float)'''
 
     print("Saving mapping...")
     with open('gene_mapping.json', 'w') as outfile:
         outfile.write(json.dumps(mapping))
 
     print("replacing gene ids")
-    links = links[:1000]
+    #links = links[:50000]
     re_links = links.replace({0: mapping})
     re_links = re_links.replace({1: mapping})
+    print(re_links)
+
+    '''driver_ids = driver.replace({0: mapping})
+    passenger_ids = passengers.replace({0: mapping})
+
+    #print(driver_ids, driver_ids[0].tolist())
+    #print()
+    #print(passenger_ids, passenger_ids[0].tolist())
+
+    #print(re_links)
+    re_links = re_links[re_links[0].isin(driver_ids[0].tolist()) & re_links[1].isin(passenger_ids[0].tolist())]
+    print(re_links)'''
+    #sys.exit()
+
     links_mat = re_links.to_numpy()
 
     # create data object
     edge_index = torch.tensor(links_mat, dtype=torch.long) 
-    combined_tr = pd.concat([driver_mask, passenger_mask])
+    '''combined_tr = pd.concat([driver_mask, passenger_mask])
     print(combined_tr)
     combined_tr = combined_tr.loc[:, 1:]
     print(combined_tr)
-    combined_tr = combined_tr.to_numpy()
-    x = torch.tensor(combined_tr, dtype=torch.float)
+    combined_tr = combined_tr.to_numpy()'''
+    x = x.loc[:, 1:]
+    x = x.to_numpy()
+    x = torch.tensor(x, dtype=torch.float)
     data = Data(x=x, edge_index=edge_index.t().contiguous())
 
     #print(gene_features[0])
-    
     print("create mask...")
-    
     #tr_mask_drivers = gene_features[0].isin(total_gene_list) #gene_features[0].isin(driver_gene_list) | gene_features[0].isin(passenger_gene_list)
     #tr_mask_drivers = torch.tensor(tr_mask_drivers, dtype=torch.bool)
     driver_gene_list.extend(passenger_gene_list)
@@ -105,12 +152,25 @@ def read_files():
     data.y = y
 
     print(data)
+    model = GCN()
+    print(model)
+    criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
 
-    # plot network
-    #G = to_networkx(data, to_undirected=True)
-    #visualize_graph(G, color=data.y)
+    for epoch in range(10):
+        loss, h = train(data, optimizer, model, criterion)
+        if epoch % 10 == 0:
+            print("Loss after {} epochs: {}".format(str(epoch), str(loss)))
 
-    
+
+
+def train(data, optimizer, model, criterion):
+    optimizer.zero_grad()  # Clear gradients.
+    out, h = model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+    return loss, h
 
 
 if __name__ == "__main__":
