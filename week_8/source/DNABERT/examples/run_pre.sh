3b1e77cfa6d44ab5d596bd38835792d23f5703f0
Anup Kumar <anup.rulez@gmail.com>
2022-07-01 13:43:48 +0200

    add train script and assemble pos neg seqs

---
+++ week_8/source/DNABERT/examples/run_pre.sh
@@ -0,0 +1,22 @@
+python3 run_finetune.py \
+    --model_type dna \
+    --tokenizer_name=dna6 \
+    --model_name_or_path ../../../pretrained_models/6-new-12w-0/ \
+    --task_name dnaprom \
+    --do_train \
+    --do_eval \
+    --data_dir sample_data/ft/6/ \
+    --max_seq_length 100 \
+    --per_gpu_eval_batch_size=32   \
+    --per_gpu_train_batch_size=32   \
+    --learning_rate 2e-4 \
+    --num_train_epochs 5.0 \
+    --output_dir ./ft/6 \
+    --evaluate_during_training \
+    --logging_steps 100 \
+    --save_steps 4000 \
+    --warmup_percent 0.1 \
+    --hidden_dropout_prob 0.1 \
+    --overwrite_output \
+    --weight_decay 0.01 \
+    --n_process 8
